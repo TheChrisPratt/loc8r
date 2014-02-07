26c38ca5df38d66bbdb10f85be8c5b5872fe0091
Chris Pratt <chris@anodyzed.com>
2014-02-07 23:37:20 +0000

    Added Static Locations List

--- server/controllers/locations.js
+++ server/controllers/locations.js
@@ -1,6 +1,6 @@
 /* GET 'home' page */
 module.exports.homelist = function (req,res) {
-	res.render("index",{ title: "Home" });
+	res.render("locations-list",{ title: "Home" });
 };
 
 /* GET 'Location Info' page */
