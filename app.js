7cdbf55f9311ecb3ac6938f41de79bcfa763a277
Chris Pratt <chris@anodyzed.com>
2014-11-02 22:01:28 -0800

    Minor

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname,"public")));
 
 // development only
-if("development" == app.get("env")) {
+if(app.get("env") == "development") {
   app.use(express.errorHandler());
 }
 
